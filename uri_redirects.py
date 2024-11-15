46376c5d5b8bd5676a6aaced1fb41ea16c67073f
saymenn <ben.aymen.1337@googlemail.com>
2024-11-15 04:23:35 +0100

    just upload code

---
+++ uri_redirects.py
@@ -0,0 +1,41 @@
+import requests
+import sys
+from concurrent.futures import ThreadPoolExecutor
+requests.packages.urllib3.disable_warnings()
+
+def location_header_check(resp):
+    status = resp.status_code
+    if status == 302 or status == 301 or status == 308:
+        headers = resp.headers
+        if "Location" in headers:
+            location = resp.headers['Location']
+        elif "location" in headers:
+            location = resp.headers['location']
+        if location:
+            if location == "https://google.com":
+                return True
+            else:
+                return False
+        else:
+            print("[-] no location header error")
+            sys.exit(0)
+
+def main(url):
+    url = url + payload
+    try:
+        resp = requests.get(url, verify = False, allow_redirects=False, timeout = 20)
+        if location_header_check(resp):
+            print("[+] potential redirect: ", url)
+        else:
+            pass
+    except Exception as error:
+        pass
+
+
+payload = "//https://google.com"
+script, urls_file = sys.argv
+
+if urls_file:
+    with open(urls_file, 'r') as urls:
+        with ThreadPoolExecutor(max_workers=50) as executor:
+            executor.map(main, (url.strip() for url in urls))
