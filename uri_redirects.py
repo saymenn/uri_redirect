e9c080dae8ffec2fad60217cc32772c385fdb840
saymenn <ben.aymen.1337@googlemail.com>
2024-11-17 23:43:00 +0100

    threds as user input

--- uri_redirects.py
+++ uri_redirects.py
@@ -31,11 +31,16 @@ def main(url):
     except Exception as error:
         pass
 
+def usage():
+    print("""
+python3 uri_redirect.py <hosts_file> <threads>
+ """)
 
 payload = "//https://google.com"
-script, urls_file = sys.argv
+
+script, urls_file, threads = sys.argv
 
 if urls_file:
     with open(urls_file, 'r') as urls:
-        with ThreadPoolExecutor(max_workers=50) as executor:
+        with ThreadPoolExecutor(max_workers=int(threads)) as executor:
             executor.map(main, (url.strip() for url in urls))
